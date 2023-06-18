93d3a79b9e4ade1b1eb089798887455fb5bedb59
Shivam <alasso.tech@gmail.com>
2023-06-19 01:19:31 +0530

    second commit

--- script.js
+++ script.js
@@ -1,21 +1,4 @@
-function toggleSensitiveContent() {
-	allowSensitiveContent = !hideSensitiveContentToggle.checked;
-	localStorage.setItem("allowSensitiveContent", allowSensitiveContent);
-	renderPosts(true);
-}
 
-function toggleSensitiveContentBlur() {
-	blurSensitiveContent = blurSensitiveContentToggle.checked;
-	localStorage.setItem("blurSensitiveContent", blurSensitiveContent);
-
-	if (!blurSensitiveContent) {
-		postsList.classList.add("disable-sensitive-content-blur");
-	} else {
-		postsList.classList.remove("disable-sensitive-content-blur");
-	}
-
-	console.log(blurSensitiveContent);
-}
 
 function scrollToTop() {
 	window.scroll({
